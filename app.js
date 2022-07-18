d9ce5f769815c9a31a1140a04a9e261f046cda01
AntonioSalazarAntunes <77238018+AntonioSalazarAntunes@users.noreply.github.com>
2022-07-18 19:03:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var links = document.querySelector(".right");
+var button = document.querySelector(".hamburger-button");
+
+button.addEventListener("click", ()=> {
+  links.classList.toggle("active");
+})
+
+
